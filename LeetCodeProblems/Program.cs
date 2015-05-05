a14d627a466785305f5af9e9e3d6f9bb7fa7224b
CoverJ <coverj@live.com>
2015-05-05 18:03:53 +1000

    Solution for #20 implemented

---
+++ LeetCodeProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeProblems.Problems;
+
+namespace LeetCodeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new _020_ValidParentheses_Easy();
+            Console.WriteLine(solution.isValid("()[]{}").ToString());
+            Console.Read();
+        }
+    }
+}
